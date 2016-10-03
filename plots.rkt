b0da2654e4713331ecfce8f2334543d9ab873e12
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2016-10-03 14:23:27 +0200

    More plots & plot code cleanup

--- plots.rkt
+++ plots.rkt
@@ -4,13 +4,29 @@
          json
          rnrs/io/ports-6)
 
-(plot3d
-  (points3d
-    (sequence-map
-      (compose
-        (λ (l) (list ('speed l) ('load l) ('fuel-cons l)))
-        string->jsexpr)
-      (in-lines (open-file-input-port "log.json")))
-    #:color 'red)
-  #:width 1000
-  #:height 1000)
+(define data-seq
+  (for/stream ([line (in-lines
+                       (open-file-input-port "log.json"))])
+              (string->jsexpr line)))
+
+(define (value-seqs data cols)
+  (for/stream ([jsobj data])
+              (map (curry hash-ref jsobj) 
+                   cols)))
+
+(define (draw-plot cols)
+  (plot3d
+    (points3d
+      (value-seqs data-seq cols)
+      #:color 'blue
+      #:fill-color 'red
+      #:sym 'fullcircle)
+    #:width 1000
+    #:height 1000
+    #:bgcolor 'lightgray
+    #:angle 313
+    #:altitude 10))
+
+(draw-plot '(speed load fuel-cons))
+(draw-plot '(temp load fuel-cons))
+(draw-plot '(rpm speed fuel-cons))
